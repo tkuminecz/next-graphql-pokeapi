b6b52f14cede8d81c6060f599b112918fb25b3d5
Tim Kuminecz <tkuminecz@gmail.com>
2020-07-21 22:39:53 -0400

    fix typo

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -5,6 +5,6 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const grahql = await graphqlServer;
-  return grahql.handleRequest(req, res);
+  const graphql = await graphqlServer;
+  return graphql.handleRequest(req, res);
 };
